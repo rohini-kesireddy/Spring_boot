c831e0ba0f2c7440c31ed089e48e2177257397eb
rohini-kesireddy <78852450+rohini-kesireddy@users.noreply.github.com>
2021-03-16 17:12:00 +0530

    Add files via upload

---
+++ DemoSampleEx/src/main/java/com/ltts/DemoSampleEx/App.java
@@ -0,0 +1,17 @@
+package com.ltts.DemoSampleEx;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    ApplicationContext context = new ClassPathXmlApplicationContext("web.xml");
+    Student s=(Student)context.getBean("stu");
+    System.out.println(s);
+    }
+}
+  
